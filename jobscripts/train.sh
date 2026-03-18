3ce41deeda26e3473a0baa6d05b93b08de269387
Markus Goetz <markus.goetz@kit.edu>
2026-03-18 09:19:31 +0100

    Modified jobscripts to contain OL_DATA_ROOT export

---
+++ jobscripts/train.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gres=gpu:4
+#SBATCH --time=2-00:00:00
+#SBATCH --job-name=optollama-train
+#SBATCH --output=train-log-%x_%j.out
+#SBATCH --error=error-log-%x_%j.err
+
+export OL_DATA_ROOT=...
+
+srun --gpu-bind=closest python -u OptoLlama/scripts/train.py --config OptoLlama/configs/optollama_horeka.yaml
+
